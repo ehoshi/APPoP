80a24ae818efa6611cd5f18d1f82d539dd47f9d2
Nicholas Mills <nlmills@g.clemson.edu>
2020-04-22 13:44:20 -0400

    Convert build system to CMake
    CMake build commands:
    
       $ mkdir build; cd build
       $ cmake -D CMAKE_BUILD_TYPE=Debug ..
       $ make
    
    The helper script makeFrame.sh is included for compiling on the Palmetto
    cluster.

---
+++ makeFrame.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+module load cmake/3.13.1
+module load armadillo
+module add gcc/6.1.0 openmpi/1.6.4-eth
+#OR module load gcc/8.2.0 openmpi/4.0.1-gcc82-ucx
+
+mkdir build
+cd build
+cmake -D "CMAKE_PREFIX_PATH=$ARMADILLO_DIR" \
+      -D "CMAKE_CXX_FLAGS=-Og" \
+      -D "CMAKE_BUILD_TYPE=Debug" \
+      ..
+make
